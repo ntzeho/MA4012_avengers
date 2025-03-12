12bac1e9027c2a2ed9c8135556c9a7f2f808358b
ntzeho <ntzeho@gmail.com>
2025-03-12 11:30:46 +0800

    add conditions for robot states, change int to short to free up memory usage

--- movement.c
+++ movement.c
@@ -1,9 +1,11 @@
-void drive(int direction, int speed) {
+void drive(short direction, short speed) {
     motor [left_motor] = speed * direction;
     motor [right_motor] = speed * direction;
 }
 
-void turn(int direction, int speed) {
+void turn(short direction, short speed) {
+    //assuming 1 is forward and -1 is backward:
+    //direction = 1 means turn right, direction = -1 means turn left
 	motor [left_motor] = speed * direction;
 	motor [right_motor] = -speed * direction;
 }
@@ -14,9 +16,21 @@ void stop_movement() {
 }
 
 void move_field() { //move to just over halfway point to begin searching for balls
-    if (time10[T1] < 12000) { //movement when total elapsed time under 2mins
-        drive(1, 30);
-        sleep(2000);
+    if (time10[T1] < 12000 && ball_count == 0) { //movement when total elapsed time under 2mins
+        drive(1, 127);
+        sleep(3000);
+    } else if (ball_count == 1) {
+        //movement to centre sector with appropriate turning depending on start_position value
+        drive(1, 127);
+        sleep(3000);
+        switch (start_position) {
+            case 'L':
+                //do xxx
+                break;
+            case 'R':
+                //do yyy
+                break;
+        }
     } else { //movement when only 1 min left on clock
         drive(1, 30);
         sleep(1000);
--- robot_state.c
+++ robot_state.c
@@ -1,3 +1,5 @@
+const short STATE_CORRECT_VALUE = 3;
+
 enum robotState {
     // line sensor
     LINE_SENSOR_DETECTED,           // line sensor detected, ignore all get back in game
@@ -13,11 +15,87 @@ enum robotState {
     BALL_COLLECTED_NO_ROBOT,        // move to home
     // limit switch - rear of robot
     HOME,                           // deposit ball
+    START,                          // start of competition
     BLANK,
 };
-robotState robot_state = BLANK;
 
-// only after 5 counts, then declare that the state is correct; this is to correct the random surges
-robotState detected_state = BLANK;
-robotState previous_detected_state = BLANK;
+robotState robot_state = START;
+
+// only after 3 counts, then declare that the state is correct; this is to correct the random surges
+robotState detected_state = START;
+robotState previous_detected_state = START;
 int state_counter = 0;
+
+void robot_state_machine() {
+    if (SensorValue [line_FL] == 1 || SensorValue [line_FR] == 1 || SensorValue [line_BL] == 1 || SensorValue [line_BR] == 1) {
+        detected_state = LINE_SENSOR_DETECTED;
+    } else if (distance_long2 < 45 && SensorValue [ball_switch] == 0 && distance_long0 > 20) {
+        detected_state = BALL_DETECTED;
+    } else if (SensorValue [bumper_L] == 1 && SensorValue [bumper_R] == 1 && distance_long1 >= 15 && SensorValue [ball_switch] == 1) {
+        detected_state = HOME;
+    } else if (SensorValue [ball_switch] == 1 && distance_long0 >= 15 && distance_long1 >= 15) {
+        detected_state = BALL_COLLECTED_NO_ROBOT;
+    } else if (SensorValue [ball_switch] == 1 && distance_long0 < 15 && distance_long1 >= 15) {
+        detected_state = ROBOT_FRONT_DETECTED_BALL_IN;
+    } else if (SensorValue [ball_switch] == 0 && distance_long0 < 15 && distance_long1 >= 15) {
+        detected_state = ROBOT_FRONT_DETECTED_BALL_OUT;
+    } else if (SensorValue [ball_switch] == 1 && distance_long0 >= 15 && distance_long1 < 15) {
+        detected_state = ROBOT_REAR_DETECTED_BALL_IN;
+    } else if (SensorValue [ball_switch] == 0 && distance_long0 >= 15 && distance_long1 < 15) {
+        detected_state = ROBOT_REAR_DETECTED_BALL_OUT;
+    } else {
+        detected_state = BALL_SEARCH_NO_ROBOT;
+    }
+
+    if (detected_state == previous_detected_state) {state_counter++;}
+    else {state_counter = 1;}
+
+    previous_detected_state = detected_state;
+    if (state_counter >= STATE_CORRECT_VALUE && robot_state != detected_state) {
+        robot_state = detected_state;
+        state_counter = 1;
+    }
+
+}
+
+/*
+void robot_state_machine() {
+    if (distance_long0 < 15) {
+        detected_state = ROBOT_FRONT_DETECTED;
+    }
+    else if (distance_long1 < 15) {
+        detected_state = ROBOT_REAR_DETECTED;
+    }
+    else if (((distance_long0 > 15)) && (distance_long2 < 40)) {
+        detected_state = BALL_DETECTED;
+        }
+    else {
+        detected_state = BLANK;
+    }
+
+    if (detected_state == previous_detected_state) {
+        state_counter++;
+    }
+    else {
+        state_counter = 1;
+    }
+
+    previous_detected_state = detected_state;
+
+    if (state_counter >= 5 && robot_state != detected_state) {
+        robot_state = detected_state;
+
+        switch(robot_state) {
+            case BALL_DETECTED:
+                break;
+            case ROBOT_FRONT_DETECTED:
+                break;
+            case ROBOT_REAR_DETECTED:
+                break;
+            case BLANK:
+                break;
+        }
+        state_counter = 1;
+    }
+}
+*/
