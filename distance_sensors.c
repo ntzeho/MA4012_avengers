c4d86503a428438e450e2d8f7b56fb54fb9c30a1
ntzeho <ntzeho@gmail.com>
2025-03-16 15:56:38 +0800

    add 50ms sleep to minimise probability of fluctations

--- distance_sensors.c
+++ distance_sensors.c
@@ -6,7 +6,6 @@ float distance_long0;   // robot front checker
 float distance_long1;   // robot rear checker
 float distance_long2;   // ball finder
 
-
 void distance_calculator() {
     voltage_long0 = (SensorValue [dist_long0]) * (5.0 / 4096.0);
     voltage_long1 = (SensorValue [dist_long1]) * (5.0 / 4096.0);
@@ -19,6 +18,8 @@ void distance_calculator() {
     if (distance_long0 > 50) {distance_long0 = 50;}
     if (distance_long1 > 50) {distance_long1 = 50;}
     if (distance_long2 > 50) {distance_long2 = 50;}
+
+    sleep(50);
 }
 
 
